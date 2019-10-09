18fa65df93e860c95e96834d2d7f2d9eb11452d1
unknown <you@example.com>
2019-10-09 20:29:18 -0300

    changes in the frontend

--- frontend/src/components/Graphs.js
+++ frontend/src/components/Graphs.js
@@ -8,27 +8,75 @@ import {
   YAxis,
   LineChart
 } from "react-timeseries-charts";
-import DateTime from "react-datetime";
-import "react-datetime/css/react-datetime.css";
+
 export class Graph extends Component {
   state = {
     mes: "",
-    ano: ""
+    ano: 2019,
+    lista: []
   };
   componentDidMount() {
     this.props.getValues();
   }
-
+  onSubmit = e => {
+    e.preventDefault();
+    this.setState({
+      // como saber se o mes escolhido esta antes ou depous do ano
+      lista: this.props.valores.filter(value => {
+        var data = new Date(value.pagar_em);
+        var mes = Number(data.getMonth()) 
+        var diferença_anos = (data.getFullYear() - this.state.ano-1) * 12;
+        console.log(tempo, diferença_anos, value.parcelas);
+        if (diferença_anos>=0 && ) {
+          console.log(value);
+          return value;
+        }
+      })
+    });
+    console.log(this.state.lista);
+  };
+  onChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
   render() {
     return (
       <div>
-        <DateTime
-          className="input-group-lg"
-          timeFormat={false}
-          defaultValue={new Date()}
-          viewMode="months"
-          onChange={this.onMax_time}
-        />
+        <form onSubmit={this.onSubmit}>
+          <div className=" container form-group form-group-sm d-flex">
+            <select
+              className="form-control input-sm "
+              name="mes"
+              value={this.state.mes}
+              onChange={this.onChange}
+            >
+              <option value={1}>janeiro</option>
+              <option value={2}>feveireiro</option>
+              <option value={3}>março</option>
+              <option value={4}>abril</option>
+              <option value={5}>maio</option>
+              <option value={6}>junho</option>
+              <option value={7}>julho</option>
+              <option value={8}>agosto</option>
+              <option value={9}>setembro</option>
+              <option value={10}>outubro</option>
+              <option value={11}>novembro</option>
+              <option value={12}>dezembro</option>
+            </select>
+            <input
+              className="form-control input-sm"
+              name="ano"
+              value={this.state.ano}
+              type="number"
+              onChange={this.onChange}
+            ></input>
+            <button type="submit"> submit</button>
+          </div>
+        </form>
+        <p>{this.state.mes}</p>
+        <p>{this.state.ano}</p>
+        {this.state.lista.map(value => {
+          <p>{value}</p>;
+        })}
       </div>
     );
   }
